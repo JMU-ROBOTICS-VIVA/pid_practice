3665fc8cc53acbc382116a470836bb32f5ef935b
Nathan Sprague <nathan.r.sprague@gmail.com>
2022-09-07 17:00:31 -0400

    updates for foxy

--- launch/rocket.launch.py
+++ launch/rocket.launch.py
@@ -6,18 +6,12 @@ from launch.actions import SetEnvironmentVariable
 
 
 def generate_launch_description():
-    # By default (in Dashing) log messages are buffered and do not
-    # immediately appear on the screen.  This is a hack to change that
-    # behavior.
-    lc = LaunchContext()
-    SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED',
-                           '1').visit(lc)
 
     return LaunchDescription([
         Node(
             package="pid_practice",
-            node_executable="guidance",
-            node_name="guidance_node",
+            executable="guidance",
+            name="guidance_node",
             output="screen",
             parameters=[
                 {"p_gain": -1.0,
@@ -27,8 +21,8 @@ def generate_launch_description():
         ),
         Node(
             package="rocketbot",
-            node_executable="rocketbot_node",
-            node_name="rocketbot_node",
+            executable="rocketbot_node",
+            name="rocketbot_node",
             output="screen")
 
     ])
